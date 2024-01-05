d19b2812e333881d0e6db1eeccd6ffd82322a698
IfeoluwaBalogun <ifeoluwabee@gmail.com>
2024-01-05 01:21:18 +0100

    implemented subtraction

---
+++ ArithmeticMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyTestConsoleProject
+{
+    internal class ArithmeticMethod
+    {
+        public void perfomSubtraction()
+        {
+            Console.WriteLine("Enter first value");
+            decimal.TryParse(Console.ReadLine(), out decimal firstValue);
+            Console.WriteLine("Enter second value");
+            decimal.TryParse(Console.ReadLine(), out decimal secondValue);
+            decimal sum = firstValue + secondValue;
+            Console.WriteLine($"\nThe difference is {decimal.Round(sum, 2)}");
+           
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,12 +8,9 @@
             Console.WriteLine("Welcome To academy 2023");
             Console.WriteLine("===================================");
             Console.WriteLine("--------------- Mini Calculation-----------");
-            Console.WriteLine("Enter first value");
-            decimal.TryParse(Console.ReadLine(),out decimal firstValue);
-            Console.WriteLine("Enter second value");
-            decimal.TryParse(Console.ReadLine(),out decimal secondValue);
-            decimal sum = firstValue + secondValue;
-            Console.WriteLine($"\nThe sum is {decimal.Round(sum, 2)}");
+
+            var arithmeticCaller = new ArithmeticMethod();
+            
             Console.ReadKey();
         }
     }
